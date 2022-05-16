029f730a2e4ce4c19fd33bd557baef2d2c39493e
Nalog00 <bisenovsaliq@gmail.com>
2022-05-16 20:06:12 +0500

    Fixed order by name by djakonystar

--- app/src/main/java/io/jamshid/navoiyterminology/data/local/dao/TermDao.kt
+++ app/src/main/java/io/jamshid/navoiyterminology/data/local/dao/TermDao.kt
@@ -2,7 +2,6 @@ package io.jamshid.navoiyterminology.data.local.dao
 
 import androidx.room.*
 import io.jamshid.navoiyterminology.data.local.entities.Term
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TermDao {
@@ -13,7 +12,7 @@ interface TermDao {
     @Update(entity = Term::class)
     suspend fun updateTerm(term: Term)
 
-    @Query("SELECT*FROM term ORDER BY term.name")
+    @Query("SELECT*FROM term ORDER BY lower(term.name)")
     suspend fun getAllTerms(): List<Term>
 
     @Query("SELECT*FROM term WHERE status=1")
